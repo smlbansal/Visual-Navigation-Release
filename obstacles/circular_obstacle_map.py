7ee6b71eebfbdccea73109b5111fab0f1e22a122
Varun Tolani <vatolani@gmail.com>
2018-09-12 14:04:43 -0700

    add create_occupancy_grid function

--- obstacles/circular_obstacle_map.py
+++ obstacles/circular_obstacle_map.py
@@ -4,51 +4,57 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 class CircularObstacleMap(ObstacleMap):
-  def __init__(self, map_bounds, min_n, max_n, min_r, max_r):
-    assert(min_r > 0 and max_r > 0)
-    x_min, y_min, x_max, y_max = map_bounds[0][0], map_bounds[0][1], map_bounds[1][0], map_bounds[1][1]
-    
-    self.m = np.random.randint(min_n, max_n+1)
-    self.map_bounds = map_bounds
-   
-    cs,rs = [],[] 
-    while len(rs) < self.m:
-      x, y = np.random.uniform(x_min, x_max), np.random.uniform(y_min, y_max)
-      r = np.random.uniform(min_r, max_r)
-      if np.sqrt(x**2+y**2) > r: #check obstacle doesn't touch origin
-        cs.append([x,y])
-        rs.append([r])
-   
-    cs, rs = np.array(cs), np.array(rs)
-    self.c_m2= tf.convert_to_tensor(cs, name='circle_centers', dtype=tf.float32)
-    self.r_m1 = tf.convert_to_tensor(rs, name='circle_radii', dtype=tf.float32) 
+    def __init__(self, map_bounds, min_n, max_n, min_r, max_r):
+        assert(min_r > 0 and max_r > 0)
+        x_min, y_min, x_max, y_max = map_bounds[0][0], map_bounds[0][1], map_bounds[1][0], map_bounds[1][1]
+
+        self.m = np.random.randint(min_n, max_n+1)
+        self.map_bounds = map_bounds
+
+        cs,rs = [],[] 
+        while len(rs) < self.m:
+            x, y = np.random.uniform(x_min, x_max), np.random.uniform(y_min, y_max)
+            r = np.random.uniform(min_r, max_r)
+            if np.sqrt(x**2+y**2) > r: #check obstacle doesn't touch origin
+                cs.append([x,y])
+                rs.append([r])
+
+        self.c_m2= tf.convert_to_tensor(np.array(cs), name='circle_centers', dtype=tf.float32)
+        self.r_m1 = tf.convert_to_tensor(np.array(rs), name='circle_radii', dtype=tf.float32) 
   
-  def dist_to_nearest_obs(self, traj_nk2):
-    with tf.name_scope('dist_to_obs'):
-      c_11m2 = tf.expand_dims(tf.expand_dims(self.c_m2, axis=0), axis=0)
-      r_11m1 = tf.expand_dims(tf.expand_dims(self.r_m1, axis=0), axis=0)
-      traj_nk12 = tf.expand_dims(traj_nk2, axis=2) 
-      
-      c_nkm2 = c_11m2 + 0.*traj_nk12
-      r_nkm = tf.squeeze(r_11m1 + 0.*c_nkm2[:,:,:,0:1])
-      traj_nkm2 = traj_nk12 + 0.*c_nkm2
-      
-      diff_nkm2 = tf.square(c_nkm2 - traj_nkm2)
-      dist_nkm = tf.sqrt(tf.reduce_sum(diff_nkm2, axis=3))-r_nkm
-      min_dist_nk = tf.reduce_min(dist_nkm, axis=2) 
-      return min_dist_nk
+    def dist_to_nearest_obs(self, pos_nk2):
+        with tf.name_scope('dist_to_obs'):
+            c_11m2 = tf.expand_dims(tf.expand_dims(self.c_m2, axis=0), axis=0)
+            r_11m1 = tf.expand_dims(tf.expand_dims(self.r_m1, axis=0), axis=0)
+            pos_nk12 = tf.expand_dims(pos_nk2, axis=2) 
 
-  def create_occupancy_grid(self):
-    raise NotImplementedError
+            c_nkm2 = c_11m2 + 0.*pos_nk12
+            r_nkm = tf.squeeze(r_11m1 + 0.*c_nkm2[:,:,:,0:1])
+            pos_nkm2 = pos_nk12 + 0.*c_nkm2
 
-  def render(self, ax):
-    for i in range(self.m):
-      c = self.c_m2[i].numpy()
-      r = self.r_m1[i][0].numpy()
-      c = plt.Circle((c[0], c[1]), r, color='b')
-      ax.add_artist(c)
-    ax.plot(0.0, 0.0, 'ro')
-    map_bounds = self.map_bounds
-    x_min, y_min, x_max, y_max = map_bounds[0][0], map_bounds[0][1], map_bounds[1][0], map_bounds[1][1]
-    ax.set_xlim([x_min, x_max])
-    ax.set_ylim([y_min, y_max])
+            diff_nkm2 = tf.square(c_nkm2 - pos_nkm2)
+            dist_nkm = tf.sqrt(tf.reduce_sum(diff_nkm2, axis=3))-r_nkm
+            min_dist_nk = tf.reduce_min(dist_nkm, axis=2) 
+            return min_dist_nk
+
+    def create_occupancy_grid(self, xs_nn, ys_nn):
+        """
+            Creates an occupancy grid where 0 and 1 represent free
+            and occupied space respectively
+        """
+        grid_nn2 = tf.stack([xs_nn, ys_nn], axis=2)
+        dists_nn = self.dist_to_nearest_obs(grid_nn2)
+        occupancy_grid_nn = tf.nn.relu(tf.sign(dists_nn*-1))
+        return occupancy_grid_nn
+
+    def render(self, ax):
+        for i in range(self.m):
+            c = self.c_m2[i].numpy()
+            r = self.r_m1[i][0].numpy()
+            c = plt.Circle((c[0], c[1]), r, color='b')
+            ax.add_artist(c)
+        ax.plot(0.0, 0.0, 'ro')
+        map_bounds = self.map_bounds
+        x_min, y_min, x_max, y_max = map_bounds[0][0], map_bounds[0][1], map_bounds[1][0], map_bounds[1][1]
+        ax.set_xlim([x_min, x_max])
+        ax.set_ylim([y_min, y_max])
--- tests/test_obstacle_map.py
+++ tests/test_obstacle_map.py
@@ -7,23 +7,34 @@ import matplotlib.pyplot as plt
 import pdb
 
 def test_circular_obstacle_map():
-  from obstacles.circular_obstacle_map import CircularObstacleMap
-  np.random.seed(seed=1)
-  n,k = 100, 20
-  map_bounds = [(-2,-2), (2,2)] #[(min_x, min_y), (max_x, max_y)]
-  min_n, max_n = 2, 4
-  min_r, max_r = .25, .5
+    from obstacles.circular_obstacle_map import CircularObstacleMap
+    np.random.seed(seed=1)
+    N=100
+    n,k = 100, 20
+    map_bounds = [(-2,-2), (2,2)] #[(min_x, min_y), (max_x, max_y)]
+    min_n, max_n = 2, 4
+    min_r, max_r = .25, .5
 
-  traj_nk2 = np.zeros((n,k,2))
-  traj_nk2 = tf.convert_to_tensor(traj_nk2, name='traj', dtype=tf.float32)
+    traj_nk2 = np.zeros((n,k,2))
+    traj_nk2 = tf.convert_to_tensor(traj_nk2, name='traj', dtype=tf.float32)
 
-  grid = CircularObstacleMap(map_bounds, min_n, max_n, min_r, max_r)
-  obs_dists_nk = grid.dist_to_nearest_obs(traj_nk2)
-  
-  fig = plt.figure()
-  ax = fig.add_subplot(111)
-  grid.render(ax)
-  plt.show()
+    grid = CircularObstacleMap(map_bounds, min_n, max_n, min_r, max_r)
+    obs_dists_nk = grid.dist_to_nearest_obs(traj_nk2)
+
+    xs = np.linspace(map_bounds[0][0], map_bounds[1][0], 100, dtype=np.float32)
+    ys = np.linspace(map_bounds[0][1], map_bounds[1][1], 100, dtype=np.float32)
+    XS, YS = tf.meshgrid(xs, ys[::-1])
+   
+    occupancy_grid_nn = grid.create_occupancy_grid(XS, YS)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(121)
+    grid.render(ax)
+
+    ax = fig.add_subplot(122)
+    ax.imshow(occupancy_grid_nn, cmap='gray')
+    ax.set_axis_off()
+    plt.show()
  
 if __name__ == '__main__':
-  test_circular_obstacle_map()
+    test_circular_obstacle_map()
