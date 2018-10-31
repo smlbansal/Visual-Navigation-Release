fix bugs in control pipelie computation

control_pipelines/control_pipeline_v0.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from control_pipelines.control_pipeline import ControlPipeline
 
 
@@ -6,12 +5,11 @@ class Control_Pipeline_v0(ControlPipeline):
     """A control pipeline in which all trajectories in the pipeline are
     valid. """
     pipeline_name = 'v0'
+    calculate_spline_speeds = False
 
     @staticmethod
-    def _compute_batch_size(params):
+    def keep_valid_problems(system_dynamics, k, planning_horizon_s,
+                            start_config, goal_config, params):
+        """All problems presented by start_config and goal_config
+        are treated as valid in Control Pipeline V0."""
         return params.n
-
-    def _compute_valid_batch_idxs(self, horizon_s):
-        """ All trajectories in the control pipeline are valid
-        for pipeline v0"""
-        return tf.range(self.params.n)

control_pipelines/control_pipeline_v1.py
@@ -11,20 +11,23 @@ class Control_Pipeline_v1(ControlPipeline):
 
     @staticmethod
     def keep_valid_problems(system_dynamics, k, planning_horizon_s,
-                                start_config, goal_config, params):
+                            start_config, goal_config, params):
         """Compute which problems are valid by constructing a spline
         from start_config to goal_config and checking dynamic feasability.
         Updates start_config and goal_config objects in place and returns
         n', the new batch size for this control pipeline."""
         traj_spline = params._spline(dt=params.dt, n=params.n, k=k,
-                                params=params)
+                                     params=params)
         ts_nk = tf.tile(tf.linspace(0., planning_horizon_s, k)[None], [params.n, 1])
         traj_spline.fit(start_config=start_config, goal_config=goal_config,
-                             factors=None)
+                        factors=None)
         traj_spline.eval_spline(ts_nk, calculate_speeds=True)
-        valid_idxs = traj_spline.check_dynamic_feasability(system_dynamics.v_bounds[1],
-                                                           system_dynamics.w_bounds[1],
-                                                           horizon_s=planning_horizon_s)
+        valid_idxs = traj_spline.enforce_dynamic_feasability(system_dynamics.v_bounds[1],
+                                                             system_dynamics.w_bounds[1],
+                                                             horizon_s=planning_horizon_s)
+
+        # Update start_config and goal_config
+        # to only hold valid problems
         start_config.gather_across_batch_dim(valid_idxs)
         goal_config.gather_across_batch_dim(valid_idxs)
         return len(valid_idxs.numpy())

trajectory/spline/spline_3rd_order.py
@@ -133,7 +133,7 @@ class Spline3rdOrder(Spline):
         self.y_coeffs_n14 = None
         self.p_coeffs_n14 = None
 
-    def check_dynamic_feasability(self, speed_max_system, angular_speed_max_system, horizon_s):
+    def enforce_dynamic_feasability(self, speed_max_system, angular_speed_max_system, horizon_s):
         """Checks whether the current computed spline (on time points in [0, horizon_s])
         can be executed in time <= horizon_s (specified in seconds) while respecting max speed and
         angular speed constraints. Returns the batch indices of all valid splines."""
@@ -156,7 +156,7 @@ class Spline3rdOrder(Spline):
             valid_mask_inv_nk = tf.cast(tf.logical_not(valid_mask_nk), dtype=tf.float32)
             valid_mask_nk = tf.cast(valid_mask_nk, dtype=tf.float32)
 
-            ts_nk = ts_nk*valid_mask_nk + valid_mask_inv_nk*cutoff_horizon_n[:,None]
+            ts_nk = ts_nk*valid_mask_nk + valid_mask_inv_nk*cutoff_horizon_n[:, None]
             self.eval_spline(ts_nk, calculate_speeds=True)
 
         return tf.cast(valid_idxs, tf.int32)