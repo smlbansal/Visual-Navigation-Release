ed27423f333b1dc982695eea599508898a8fe73a
Varun Tolani <vatolani@gmail.com>
2019-01-04 16:48:53 -0800

    add other resnet preprocessing functions

--- training_utils/data_processing/normalize_images.py
+++ training_utils/data_processing/normalize_images.py
@@ -3,6 +3,8 @@ import numpy as np
 IMAGENET_MEAN_1113 = np.array([[[[0.485, 0.456, 0.406]]]], dtype=np.float32)
 IMAGENET_STD_1113 = np.array([[[[0.229, 0.224, 0.225]]]], dtype=np.float32)
 
+IMAGENET_MEAN_INT_1113 = np.array([[[[103.939, 116.779, 123.68]]]], dtype=np.float32)
+
 
 def rgb_normalize(raw_data):
     """
@@ -14,23 +16,32 @@ def rgb_normalize(raw_data):
 
 def resnet50_normalize(raw_data):
     """
-    Swap image channels RGB -> BGR
-    Normalize channels by the imagenet mean
+    Normalize pixel values to [-1, 1] range
     """
 
     raw_data['img_nmkd'] = raw_data['img_nmkd']/127.5
     raw_data['img_nmkd'] -= 1.
 
-    # Converr RGB to BGR
-    #raw_data['img_nmkd'] = raw_data['img_nmkd'][..., ::-1]
-
-    # Normalize by the imagenet mean
-    #imagenet_mean_bgr_1113 = [103.939, 116.779, 123.68]
-
-    # Normalize rgb images to [0, 1]
-    #raw_data['img_nmkd'] = raw_data['img_nmkd']/255.
-
-    # Standardize the images by imagenet mean and std dev
-    #raw_data['img_nmkd'] = (raw_data['img_nmkd'] - IMAGENET_MEAN_1113) / IMAGENET_STD_1113
     return raw_data
 
+def resnet50_normalize_v1(raw_data):
+    """
+    Normalize image by imagenet statistics
+    """
+    
+    raw_data['img_nmkd'] = raw_data['img_nmkd']/255.
+    raw_data['img_nmkd'] = (raw_data['img_nmkd']-IMAGENET_MEAN_1113) / IMAGENET_STD_1113 
+
+    return raw_data
+
+def resnet50_normalize_v2(raw_data):
+    """
+    Caffe normalization. Switch RGB to BGR
+    and normalize by imagenet statistics
+    """
+  
+    # rgb to bgr
+    raw_data['img_nmkd'] = raw_data['img_nmkd'][:, :, :, ::-1]
+    raw_data['img_nmkd'] = (raw_data['img_nmkd']-IMAGENET_MEAN_INT_1113) 
+
+    return raw_data
--- training_utils/data_processing/rgb_preprocess_resnet_50.py
+++ training_utils/data_processing/rgb_preprocess_resnet_50.py
@@ -1,4 +1,4 @@
-from training_utils.data_processing.normalize_images import resnet50_normalize
+from training_utils.data_processing.normalize_images import resnet50_normalize_v2, resnet50_normalize_v1, resnet50_normalize
 from copy import deepcopy
 
 
@@ -6,9 +6,33 @@ def preprocess(raw_data):
     # make a copy of the data
     raw_data = deepcopy(raw_data)
 
-    # normalize rgb images to [0, 1] then
-    # subtact the imagenet mean and std dev
+    # normalize rgb images to [-1, 1]
     raw_data = resnet50_normalize(raw_data)
 
     # TODO: Add data augmentation here
     return raw_data
+
+
+def preprocess_v1(raw_data):
+    # make a copy of the data
+    raw_data = deepcopy(raw_data)
+
+    # normalize rgb images to [0, 1] and then
+    # normalize by the imagenet statistics
+    raw_data = resnet50_normalize_v1(raw_data)
+
+    # TODO: Add data augmentation here
+    return raw_data
+
+
+def preprocess_v2(raw_data):
+    # make a copy of the data
+    raw_data = deepcopy(raw_data)
+
+    # caffe normalization: swap image channels (rgb -> bgr)
+    # normalize by the imagenet statistics
+    raw_data = resnet50_normalize_v2(raw_data)
+
+    # TODO: Add data augmentation here
+    return raw_data
+
