bc5cdf0124e18df7f6e88a3f7025a0dcce31fbbb
Varun Tolani <vatolani@gmail.com>
2019-01-25 13:27:41 -0800

    stashing real robot code

--- params/system_dynamics/turtlebot_dubins_v2_params.py
+++ params/system_dynamics/turtlebot_dubins_v2_params.py
@@ -1,20 +1,20 @@
-from utils import utils
 from dotmap import DotMap
-from systems.dubins_v2 import DubinsV2
+from systems.turtlebot_dubins_v2 import TurtlebotDubinsV2
 
 
 def create_params():
     p = DotMap()
-    p.system = DubinsV2
+    p.system = TurtlebotDubinsV2
     p.dt = .05
     p.v_bounds = [0.0, .6]
     p.w_bounds = [-1.1, 1.1]
-    
-    p.simulation_params = DotMap(simulation_mode='ideal',
+
+    p.simulation_params = DotMap(simulation_mode='realistic',
                                  noise_params = DotMap(is_noisy=False,
                                                        noise_type='uniform',
                                                        noise_lb=[-0.02, -0.02, 0.],
                                                        noise_ub=[0.02, 0.02, 0.],
                                                        noise_mean=[0., 0., 0.],
                                                        noise_std=[0.02, 0.02, 0.])
+
     return p
