106714d38c6a74e707060abdc3d6cb7b766fa515
Varun Tolani <vatolani@gmail.com>
2019-04-12 10:56:24 -0700

    fix script to filter successful goals and add last step information

--- executables/one_time_scripts/include_the_last_step_data.py
+++ executables/one_time_scripts/include_the_last_step_data.py
@@ -2,11 +2,11 @@ import pickle
 import numpy as np
 import os
 
-trajectory_segments_per_episode = 14
-file_folder = '/home/ext_drive/somilb/data/training_data/sbpd/sbpd_projected_grid_corrected_last_step'
-area_name = 'area5a'
-num_files = 70
-new_file_folder = '/home/ext_drive/somilb/data/training_data/sbpd/sbpd_projected_grid_include_last_step_successful_goals_only'
+trajectory_segments_per_episode = 40
+file_folder = '/home/ext_drive/somilb/data/training_data/sbpd/max_fmm_dist_20/sbpd_projected_grid'
+area_name = 'area4'
+num_files = 99
+new_file_folder = '/home/ext_drive/somilb/data/training_data/sbpd/max_fmm_dist_20/sbpd_projected_grid_include_last_step_successful_goals_only'
 
 # Store metadata
 store_metadata = True
@@ -50,7 +50,7 @@ def load_and_correct_pickle_files():
             # Find the successful episodes
             indices_for_successful_episodes = np.where(data['episode_type_string_n1'] == 'Success')[0]
             successful_episodes_numbers = np.unique(data['episode_number_n1'][indices_for_successful_episodes])
-            
+          
             # Only keep the data corresponding to the successful episodes in the main fields
             data = keep_the_successful_episodes_data(data, other_data_keys, indices_for_successful_episodes)
             
@@ -61,12 +61,21 @@ def load_and_correct_pickle_files():
             data = append_the_last_segment_data_and_delete_last_step_keys(data, other_data_keys)
             
             # Do some assertion checks
+            indices_for_timeout_episodes = np.where(data['episode_type_string_n1'] == 'Timeout')[0]
+            indices_for_collision_episodes = np.where(data['episode_type_string_n1'] == 'Collision')[0]
+
+            timeout_episodes_numbers = np.unique(data['episode_number_n1'][indices_for_timeout_episodes])
+
+            # There may be a couple of episodes with collisions
+            # now that we sample problems with larger distances (up to 20 meters)
             number_of_successful_episodes = successful_episodes_numbers.shape[0]
-            number_of_timeout_episodes = (data['episode_number_n1'][-1] - data['episode_number_n1'][0] + 1) - \
-                                         number_of_successful_episodes
-            assert data['vehicle_state_nk3'].shape[0] == \
-                   data['episode_number_n1'].shape[0] - number_of_timeout_episodes * (trajectory_segments_per_episode-1) + number_of_successful_episodes
+            number_of_timeout_episodes = timeout_episodes_numbers.shape[0]
+            num_collision_data_pts = indices_for_collision_episodes.shape[0]
             
+            assert data['vehicle_state_nk3'].shape[0] == \
+                   data['episode_number_n1'].shape[0] - number_of_timeout_episodes * \
+                   (trajectory_segments_per_episode-1) + \
+                   number_of_successful_episodes - num_collision_data_pts
         else:
             # Just append the last segment data
             data = append_the_last_segment_data_and_delete_last_step_keys(data, other_data_keys)
@@ -102,9 +111,15 @@ def append_the_last_segment_data_and_delete_last_step_keys(data, data_keys_to_ap
         last_step_key = 'last_step_' + key
         # This if-else conditions are implemented because of a bug in the collection of last step data
         if key in ['goal_position_n2', 'goal_position_ego_n2']:
-            data[key] = np.concatenate((data[key], data[last_step_key][:, 0, 0:2]), axis=0)
+            try:
+                data[key] = np.concatenate((data[key], data[last_step_key][:, 0, 0:2]), axis=0)
+            except IndexError:
+                data[key] = np.concatenate((data[key], data[last_step_key]), axis=0)
         elif key in ['optimal_waypoint_n3', 'optimal_waypoint_ego_n3']:
-            data[key] = np.concatenate((data[key], data[last_step_key][:, 0]), axis=0)
+            try:
+                data[key] = np.concatenate((data[key], data[last_step_key][:, 0]), axis=0)
+            except ValueError:
+                data[key] = np.concatenate((data[key], data[last_step_key]), axis=0)
         else:
             data[key] = np.concatenate((data[key], data[last_step_key]), axis=0)
         del data[last_step_key]
