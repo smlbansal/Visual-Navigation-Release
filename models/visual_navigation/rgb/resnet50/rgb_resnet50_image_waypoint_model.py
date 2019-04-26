9e8fb9e57e21fa711810ec234c5fc065e9379356
Varun Tolani <vatolani@gmail.com>
2019-04-26 11:33:29 -0700

    remove image waypoint model

--- models/visual_navigation/rgb/resnet50/rgb_resnet50_image_waypoint_model.py
+++
@@ -1,9 +0,0 @@
-from models.visual_navigation.rgb.resnet50.base import Resnet50ModelBase
-from models.visual_navigation.image_waypoint_model import VisualNavigationImageWaypointModel
-
-
-class RGBResnet50ImageWaypointModel(Resnet50ModelBase, VisualNavigationImageWaypointModel):
-    """
-    A model that regresses upon optimal waypoints (in the image space) given an rgb image.
-    """
-    name = 'RGB_Resnet50_Image_Waypoint_Model'
